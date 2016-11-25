0a1c80705cd227b7adf4588947d5134f0a5ac36d
Sivakumar S <sivatorose@gmail.com>
2016-11-25 21:21:36 +0530

    Update MainActivity.java

--- app/src/main/java/sivarock/android/sivafrag/MainActivity.java
+++ app/src/main/java/sivarock/android/sivafrag/MainActivity.java
@@ -52,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-
+    // Action bar menu part
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
